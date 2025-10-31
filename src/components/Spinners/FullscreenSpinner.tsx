114b2d589d5ce364b87202c97588e52298bc631a
davidedotzero <larron959@gmaiil.com>
2025-10-31 17:48:17 +0700

    no more spinner flashbang

--- src/components/Spinners/FullscreenSpinner.tsx
+++ src/components/Spinners/FullscreenSpinner.tsx
@@ -1,7 +1,7 @@
 function FullscreenSpinner() {
     return (
         <>
-            <div className="fixed inset-0 bg-black/40 z-[9999] flex justify-center items-center">
+            <div className="fixed inset-0 z-[9999] flex justify-center items-center">
                 <div className="animate-spin h-12 w-12 border-4 border-gray-200 border-t-blue-500 rounded-full"></div>
             </div>
         </>
